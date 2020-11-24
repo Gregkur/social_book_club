5154f9623ded1690cf11c861a33af1f97af15998
Lorenz Cronenberg <lorenz.cronenberg@gmx.de>
2020-11-24 16:52:54 +0100

    add validations to model for create book form

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,11 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many_attached :photos, dependent: :destroy
+
+  validates :title, presence: true, length: { minimum: 2, too_short: "minimum is %{count} characters" }
+  validates :author, presence: true, length: { minimum: 2, too_short: "minimum is %{count} characters" }
+  validates :genre, presence: true
+  validates :description, presence: true, length: { minimum: 5, too_short: "minimum is %{count} characters" }
+  validates :year, presence: true
+  validates :pages, presence: true, numericality: { only_integer: true }
 end
