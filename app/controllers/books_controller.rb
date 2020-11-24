465a635911ec1db9b2ed7caed83d1a3d7757c81f
Lorenz Cronenberg <lorenz.cronenberg@gmx.de>
2020-11-24 15:56:16 +0100

    setup controller using pundit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,28 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:new, :create]
   def new
-
+    @book = current_user.books.new
+    authorize @book
   end
 
   def create
+    @book = current_user.books.new(book_params)
+    authorize @book
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :genre, :description, :year, :pages, photos: [])
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
+    authorize @book
   end
 end
