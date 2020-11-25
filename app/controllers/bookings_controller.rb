ecc6c4d6b1ed4cb5df81e75837eb6168d2ee5cf4
Lorenz Cronenberg <lorenz.cronenberg@gmx.de>
2020-11-25 18:32:42 +0100

    implement flatpickr

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,8 +11,10 @@ class BookingsController < ApplicationController
       flash[:alert] = "You cannot create a booking for your own book."
       render "books/show"
     else
-      @start_date = Date.new(params['booking']['from(1i)'].to_i, params['booking']['from(2i)'].to_i, params['booking']['from(3i)'].to_i)
-      @end_date = Date.new(params['booking']['until(1i)'].to_i, params['booking']['until(2i)'].to_i, params['booking']['until(3i)'].to_i)
+      @start_date = Date.parse(params[:from])
+      @end_date = Date.parse(params[:until])
+      # @start_date = Date.new(params['booking']['from(1i)'].to_i, params['booking']['from(2i)'].to_i, params['booking']['from(3i)'].to_i)
+      # @end_date = Date.new(params['booking']['until(1i)'].to_i, params['booking']['until(2i)'].to_i, params['booking']['until(3i)'].to_i)
       # @days = (end_date - start_date).to_i + 1
       @booking = Booking.new(booking_params)
       @booking.book = @book
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,3 +32,6 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+// Flat picker
+import "../plugins/flatpickr"
