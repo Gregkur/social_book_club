f410e90470a2b38e919c4c639b4d91fde9e15ec8
Lorenz Cronenberg <lorenz.cronenberg@gmx.de>
2020-11-25 12:40:37 +0100

    add edit and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :books, only: [:new, :create, :index, :show] do
+  resources :books, only: [:new, :create, :index, :show, :edit, :update] do
   end
 end
