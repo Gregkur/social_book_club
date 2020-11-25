a65b5250837513c69354373dedf1ebeda1c02299
Pia von Heyer <pia@vonheyer.de>
2020-11-25 11:33:05 +0100

    finished basic show skeleton, links missing

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def show
     @book = Book.find(params[:id])
+    authorize @book
     # @booking = Booking.new
     # @booking_created = false
     # @booking_created = true if params[:booking_created]
