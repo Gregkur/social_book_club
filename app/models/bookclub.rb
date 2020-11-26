070e1b13a3a10ab058239a19d1478ae8a99d6928
Lorenz Cronenberg <lorenz.cronenberg@gmx.de>
2020-11-26 13:44:37 +0100

    add validations to bookclub model

--- app/models/bookclub.rb
+++ app/models/bookclub.rb
@@ -2,5 +2,6 @@ class Bookclub < ApplicationRecord
   belongs_to :user, optional: true
   has_many :bookclub_users
   has_many :members, through: :bookclub_users, source: :user
-  #:members, through: :bookclub_users, class_name: "User", foreign_key: "bookclub_id"
+
+  validates :name, presence: true, uniqueness: true, length: { minimum: 2, too_short: "minimum is %{count} characters" }
 end
