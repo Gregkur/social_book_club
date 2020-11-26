51ff0b017ad8c674e7059cdef1299a8ad54e674b
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-11-26 15:44:20 +0100

    added "about" to every user so its displayed on the profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_142320) do
+ActiveRecord::Schema.define(version: 2020_11_26_143002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,6 +131,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_142320) do
     t.string "nickname"
     t.string "address"
     t.boolean "admin"
+    t.text "about"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
