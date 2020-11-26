5d81bf1ddb08c493e58afad95485c857af0476c5
Pia von Heyer <pia@vonheyer.de>
2020-11-26 15:18:23 +0100

    backend reviews done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
+  before_action :set_book, only: [:new, :create, :edit, :update]
   def create
-    @book = Book.find(params[:book_id])
     @review = Review.new(review_params)
     @review.user = current_user
     authorize @review
@@ -12,12 +12,32 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    @review = Review.find(params[:id])
+    authorize @review
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    authorize @review
+    @review.update(review_params)
+    redirect_to book_path(@review.book)
+
+  end
+
   def destroy
-    @review.destroy
+    @review = Review.find(params[:book_id])
+    authorize @review
+    @book = Book.find(params[:id])
+    @review.delete
     redirect_to book_path(@book)
   end
 
   private
+
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
 
   def review_params
     params.require(:review).permit(:book_rating, :book_comment)
