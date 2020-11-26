27ffc4bc25afe1dd4d6b24bf11e07eede7bac6bd
Lorenz Cronenberg <lorenz.cronenberg@gmx.de>
2020-11-26 18:42:52 +0100

    fix bug index

--- app/policies/book_policy.rb
+++ app/policies/book_policy.rb
@@ -1,14 +1,14 @@
 class BookPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.where(user: user)
+      scope.all
     end
   end
 
   def index?
     return true
   end
-  
+
   def create?
     true
   end
