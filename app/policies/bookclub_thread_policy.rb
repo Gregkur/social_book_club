99a1af8408bbf031c3452461831ab90784042dfe
Lorenz Cronenberg <lorenz.cronenberg@gmx.de>
2020-11-30 11:18:21 +0100

    add policies for BookclubThreads

---
+++ app/policies/bookclub_thread_policy.rb
@@ -0,0 +1,31 @@
+class BookclubThreadPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    return true
+  end
+
+  def create?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+  record.user == user
+  end
+end
