1b5e80e4df3d954aabd170a3077141c91341a48f
Lorenz Cronenberg <lorenz.cronenberg@gmx.de>
2020-11-30 11:43:04 +0100

    implementation new + create action

--- app/controllers/bookclub_threads_controller.rb
+++ app/controllers/bookclub_threads_controller.rb
@@ -2,20 +2,31 @@ class BookclubThreadsController < ApplicationController
   skip_before_action :authenticate_user!
   before_action :set_thread, only: [:new, :edit, :update, :destroy]
 
-  def create
-
+  def new
+    @bookclub_thread = BookclubThread.new(thread_params)
+    authorize @bookclub_thread
   end
 
+  def create
+    @bookclub_thread = BookclubThread.new(thread_params)
+    authorize @bookclub_thread
+    @bookclub_thread.user = current_user
+    if @bookclub_thread.save
+      redirect_to bookclub_path(@bookclub)
+      flash[:notice] = "Your thread was successfully posted!"
+    else
+      render :new
+    end
+  end
 
   private
 
-  def set_thread
-    @bookclub_thread = BookclubThread.find(params[:bookclub_thread])
-    authorize @bookclub_thread
+  def set_bookclub
+    @bookclub = Bookclub.find(params[:bookclub_id])
+    authorize @bookclub
   end
 
   def thread_params
     params.require(:bookclub_thread).permit(:title, :content)
-
   end
 end
