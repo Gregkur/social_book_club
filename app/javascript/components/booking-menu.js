8a6bce35d8f9f4e49ed231d849ce2b4658a9ef4e
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-12-01 17:37:02 +0100

    fixed js

--- app/javascript/components/booking-menu.js
+++ app/javascript/components/booking-menu.js
@@ -3,29 +3,31 @@ import 'sweetalert2/src/sweetalert2.scss'
 
 const initBookingMenu = () => {
     const bookings = document.querySelectorAll("#booking-menu")
-    bookings.forEach(booking => 
-      booking.addEventListener('click', () => {
-          Swal.fire({
-              title: 'What do you want to do?',
-              icon: 'question',
-              confirmButtonText: `View your book`,
-              denyButtonText: `Cancel your booking`,
-              showDenyButton: true,
-              showCloseButton: true,
-              allowOutsideClick: true,
-              confirmButtonColor: '#88CE8F',
-              denyButtonColor: '#E14F52',
-          }).then((result) => {
-              /* Read more about isConfirmed, isDenied below */
-              if (result.isConfirmed) {
-                  const link1 = document.querySelector('#book-link');
-                  link1.click();
-                } else if (result.isDenied) {
-                  const link2 = document.querySelector('#bookings-link');
-                  link2.click();
-                }
+    if (bookings === undefined || bookings.length == 0) {
+      bookings.forEach(booking => 
+        booking.addEventListener('click', () => {
+            Swal.fire({
+                title: 'What do you want to do?',
+                icon: 'question',
+                confirmButtonText: `View your book`,
+                denyButtonText: `Cancel your booking`,
+                showDenyButton: true,
+                showCloseButton: true,
+                allowOutsideClick: true,
+                confirmButtonColor: '#88CE8F',
+                denyButtonColor: '#E14F52',
+            }).then((result) => {
+                /* Read more about isConfirmed, isDenied below */
+                if (result.isConfirmed) {
+                    const link1 = document.querySelector('#book-link');
+                    link1.click();
+                  } else if (result.isDenied) {
+                    const link2 = document.querySelector('#bookings-link');
+                    link2.click();
+                  }
+            })
           })
-        })
-      )
+        )
+    }
 };
 export { initBookingMenu };
--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,9 +1,16 @@
 import flatpickr from "flatpickr";
 
-flatpickr(".datepicker", {
-  altInput: true,
-  altFormat: "F j, Y",
-  dateFormat: "Y-m-d",
-  minDate: "today",
-  // enableTime: true,
-});
+const initFlatPicker = () => {
+  const flatpickr = document.querySelector('.datepickr')
+    if (flatpickr) {
+      flatpickr(".datepicker", {
+        altInput: true,
+        altFormat: "F j, Y",
+        dateFormat: "Y-m-d",
+        minDate: "today",
+        // enableTime: true,
+      });
+    }
+}
+
+export { initFlatPicker }
