2bff858457a5ac38467d947b0602500ff7cf9417
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-12-01 15:35:40 +0100

    bugfixes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -43,24 +43,24 @@ document.addEventListener('turbolinks:load', () => {
     dateFormat: "Y-m-d",
     minDate: "today",
   });
+  //vars
+    const nav = document.querySelector(".header__nav");
+    const hamburger = document.querySelector(".hamburger");
+    const burger = document.getElementById('hamburger2')
+    const body = document.querySelector('body')
+  
+  // Menu slide
+    hamburger.addEventListener("click", function() {
+      hamburger.classList.toggle("is-active");
+        if (hamburger.classList.contains("is-active")){
+          burger.checked = true
+        }else{
+          burger.checked = false
+        }
+      nav.classList.toggle("overflow");
+      body.classList.toggle("overflow-x")
 // Swal for joining
   initJoinMenu()
-//vars
-  const nav = document.querySelector(".header__nav");
-  const hamburger = document.querySelector(".hamburger");
-  const burger = document.getElementById('hamburger2')
-  const body = document.querySelector('body')
-
-// Menu slide
-  hamburger.addEventListener("click", function() {
-    hamburger.classList.toggle("is-active");
-      if (hamburger.classList.contains("is-active")){
-        burger.checked = true
-      }else{
-        burger.checked = false
-      }
-    nav.classList.toggle("overflow");
-    body.classList.toggle("overflow-x")
 
   });
 
